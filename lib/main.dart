653925f7f9be61d8ed92aa0c72a5c7c73168e70b
Maksym Rosokha <maksimrosoha6@gmail.com>
2026-03-24 17:26:40 +0100

    lab_04 krakflow task list

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,91 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final List<Task> tasks = [
+    Task(title: "Write unit tests", deadline: "next week", done: true, priority:"important"),
+    Task(title: "Fix UI bugs", deadline: "today", done: false, priority:"not important"),
+    Task(title: "Prepare presentation", deadline: "Friday", done: false, priority:"very important"),
+    Task(title: "Review code", deadline: "this week", done: false, priority:"important"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("KrakFlow")),
-        body: Center(
-          child: Column(
-            children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
-            ],
-          ),
+        body: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Today you have ${tasks.length} tasks",
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Center(
+                    child: Text(
+                      "Today tasks:",
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            TaskCard(
+              title: tasks[0].title,
+              subtitle: "Subtitle for TaskCard1",
+              icon: Icons.check,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+  final String priority;
+
+  Task({
+    required this.title,
+    required this.deadline,
+    required this.done,
+    required this.priority,
+  });
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(subtitle),
         ),
       ),
     );
